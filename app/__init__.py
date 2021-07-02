6de57c633006174dcf2777a0c4f15145d0a596b7
Dipta Dwiyantoro <dipta004@gmail.com>
2021-07-02 16:13:25 +0700

    Done create api and service for sso auth with registration completion

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
-from functools import wraps
 from flask import Flask
 from flask_cors import CORS
 from flask_mongoengine import MongoEngine
+import json
 
 from app.views.auth import router_auth
 from app.views.main import router_main
 from app.cron import cron
 from uploader.views import router_uploader
 
+from pathlib import Path
+
+base_dir = Path(__file__).resolve().parent.parent
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -18,6 +21,13 @@ app.config["SECRET_KEY"] = "password"
 app.config["ACTIVE_PERIOD"] = "2018-2"
 app.config["SSO_UI_FORCE_HTTPS"] = False
 app.config["MAX_CONTENT_LENGTH"] = 2 * 1024 * 1024
+app.config["BASE_DIR"] = base_dir
+
+with open(base_dir / "sso" / "faculty-base-additional-info.json") as f:
+    app.config["FACULTY_KD_ORG"] = json.load(f)
+
+with open(base_dir / "sso" / "additional-info.json") as f:
+    app.config["BASE_KD_ORG"] = json.load(f)
 
 # uploader
 app.config['UPLOAD_FOLDER'] = "__files__"
--- app/services/auth/auth.py
+++ app/services/auth/auth.py
@@ -1,9 +1,23 @@
 import uuid
 
-from app import app
+from app.exceptions.auth import UserNotFound, KdOrgNotFound
 from app.utils import generate_token
+from app.views.main import get_app_config
 from models.major import Major
 from models.user import User
+from pydantic import BaseModel, validator, constr
+
+
+class AuthCompletionData(BaseModel):
+    completion_id: uuid.UUID
+    npm: constr(max_length=10,min_length=10)
+    kd_org: str
+
+    @validator("npm")
+    def npm_must_be_numeric_only_and_minimum_10(cls, v: str):
+        if not v.isnumeric():
+            raise ValueError("npm must be numeric")
+        return v
 
 
 class AuthServices:
@@ -54,4 +68,40 @@ class AuthServices:
             "token": token
         }
         return result
+
+    @classmethod
+    def process_auth_completion(cls,data: AuthCompletionData) -> dict:
+        user = User.objects(completion_id=data.completion_id).first()
+        if user is None:
+            raise UserNotFound()
+        base_kd_org_data = get_app_config("BASE_KD_ORG")
+        try:
+            kd_org_data = base_kd_org_data[data.kd_org]
+        except KeyError:
+            raise KdOrgNotFound()
+
+        major = Major.objects(kd_org=data.kd_org).first()
+        if major is None:
+            major = Major(name=kd_org_data["study_program"], kd_org=data.kd_org)
+            major.save()
+
+        user.npm = data.npm
+        user.major = major
+        user.completion_id = None
+        user.batch = f"20{data.npm[:2]}"
+        user.save()
+
+        token = generate_token(user.id, user.major.id)
+        result = {
+            "user_id": str(user.id),
+            "major_id": str(major.id),
+            "token": token
+        }
+        return result
+
+
+
+
+
+
 
